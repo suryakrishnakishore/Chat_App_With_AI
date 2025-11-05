db53e59409906bcc5258808428537714a12ea13b
suryakrishnakishore <suryakrishnakishore@gmail.com>
2025-11-05 12:02:45 +0530

    Added background for message container

--- src/components/home/conversation.tsx
+++ src/components/home/conversation.tsx
@@ -3,12 +3,21 @@ import ConversationHeader from '../conversation-header';
 import { useConversationStore } from '@/store/chat-store';
 import MessageContainer from '../message-container';
 import MessageInput from '../message-input';
+import bgLight from '../../../public/bg-light.png';
+import bgDark from '../../../public/bg-dark.png';
+import { useTheme } from 'next-themes';
 
 const Conversation = () => {
   const { selectedConversation } = useConversationStore();
+  const { theme } = useTheme();
 
+  const backgroundImage = theme === 'dark' ? bgDark.src : bgLight.src;
   return (
-    <div className="flex flex-col h-full bg-[hsl(var(--chat-tile-light))] dark:bg-[hsl(var(--chat-tile-dark))] rounded-lg shadow-lg">
+    <div className="flex flex-col h-full rounded-lg shadow-lg bg-cover bg-center transition-all duration-300"
+         style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
       <ConversationHeader conversation={selectedConversation} />
       <div className="flex-1 overflow-y-auto custom-scrollbar">
         <MessageContainer />
